d8926355465b670e095af5eff4f0fb27d2fa295d
RobertRFG <robert.rndm.fg@gmail.com>
2025-07-30 19:00:45 +0200

    Major updates and revamps.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
 document.getElementById("copyright").innerText = "2025";
+
+const button = document.querySelector('.dropdown_button');
+const menu = document.querySelector('.navbar_menu');
+
+button.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', () => {
+    menu.style.display = 'none';
+});
